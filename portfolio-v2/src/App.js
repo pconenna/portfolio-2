3f7bd024308718fd38fdad15203bf7e07664e8cc
Peter Conenna <pconenna@depaul.edu>
2023-08-18 16:41:12 -0500

    finished refactoring

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from "./components/Header";
 import About from "./components/About";
 import Work from "./components/Work";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
     <Header/>
     <About/>
     <Work/>
+    <Footer/>
     </div>
   );
 }
---
+++ portfolio-v2/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+function Footer(){
+    return (
+        <footer>
+        <section id="contact-me">
+            <h2>Contact Me</h2>
+            <a href="mailto:peterconenna13@gmail.com">Email</a>
+            <a href="https://www.linkedin.com/in/peterjconenna/">LinkedIn</a>
+        </section>
+            <a href="#page_top">Back to top</a>
+
+        </footer>
+    )
+}
+export default Footer;
