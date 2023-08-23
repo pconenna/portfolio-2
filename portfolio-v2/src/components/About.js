0ff80bfdfc2fbef258eed4d362725c5b2452fd73
Peter Conenna <pconenna@depaul.edu>
2023-08-23 15:50:08 -0500

    added resume to about section and chagned work section layout

--- portfolio-v2/src/components/About.js
+++ portfolio-v2/src/components/About.js
@@ -10,6 +10,7 @@ function About(){
             <p id="intro">My name is Peter Conenna. I have a bachelor's degree in IT from Depaul University.
                 I am a student in Northwestern University's SPS full stack coding bootcamp.
                 I would consider myself an aspiring full stack web developer. </p>
+            <p>View and download my resume <a href='https://docs.google.com/document/d/1JviH35cMUQXjcMWvtZF7j8Trn3y4-9yJ1990ZR0dlDc/edit?usp=sharing'>here</a>.</p>
         </section>
     </section>
     )
--- portfolio-v2/src/components/Work.js
+++ portfolio-v2/src/components/Work.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import '../App.css';
-import placeholder from '../images/placeholder.jpg';
 
 import bol2 from '../images/bol2.PNG';
 import mm from '../images/mm_demo.PNG';
@@ -9,41 +8,37 @@ import pt from '../images/playtrade.PNG';
 
 function Work(){
     return (
-        <section id="my-work">
-        <h2>My Work</h2>
-        <section className="images">
-        
-       <figure id="bol">
-        <a href="https://barksoflove.org/index.html"><img src={bol2}/></a>
-        <figcaption className="bol-title">barksoflove.org</figcaption> 
-       </figure>
+        <div className='container text-center'>
+            <div className='row'>
+                <div className='col'>
+                    <figure id="bol">
+                    <a href="https://barksoflove.org/index.html"><img src={bol2}/></a>
+                    <figcaption className="bol-title">barksoflove.org</figcaption> 
+                    </figure>
+                </div>
+                <div className='col'>Website for all operations of Barks of Love Animal Rescue. </div>
+            </div>
 
-        <figure class="bottom-fig">
-            <a href="https://project01team02.github.io/movie-mixers/index.html"><img src={mm}/></a> 
-        <figcaption className="title">Movie Mixers</figcaption>
-        </figure> 
+            <div className='row'>
+            <div className='col'>Movie Mixers </div>
+                <div className='col'>
+                <figure class="bottom-fig">
+                <a href="https://project01team02.github.io/movie-mixers/index.html"><img src={mm}/></a> 
+                <figcaption className="title">Movie Mixers</figcaption>
+                </figure> 
+                </div>
+            </div>
 
-          
-        <figure className="bottom-fig">
-            
-        <a href="https://play-trade-3e0a6365bb54.herokuapp.com/"><img src={pt}/></a>
-        <figcaption className="title">Playtrade</figcaption>
-
-        </figure>
-        <figure className="bottom-fig">
-            
-        <img  src={placeholder}/> 
-        <figcaption className="title">Placeholder</figcaption>
-
-        </figure>
-        <figure className="bottom-fig">
-            
-        <img  src={placeholder}/>
-        <figcaption className="title">Placeholder</figcaption>
-
-        </figure>
-    </section> 
-    </section>
+            <div className='row'>
+                <div className='col'>
+                <figure className="bottom-fig">
+                <a href="https://play-trade-3e0a6365bb54.herokuapp.com/"><img src={pt}/></a>
+                <figcaption className="title">Playtrade</figcaption>
+                </figure>
+                </div>
+                <div className='col'>Website for all operations of Barks of Love Animal Rescue. </div>
+            </div>
+        </div>
     )
 }
 export default Work;
